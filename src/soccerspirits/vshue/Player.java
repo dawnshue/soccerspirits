502db1f30fe14446f4fa963e1ef7b014cd3d586d
Vangie Shue <vangieshue@gmail.com>
2015-12-21 22:42:35 -0500

    Completed csv, writing service & player

--- src/soccerspirits/vshue/Player.java
+++ src/soccerspirits/vshue/Player.java
@@ -1,5 +1,109 @@
 package soccerspirits.vshue;
 
+
 public class Player {
 
+	public final static int NUM_FIELDS = 5;
+
+	public enum Element {
+		FIRE, WIND, THUNDER, LIGHT, DARK
+	}
+
+	private String name;
+	private boolean isHas;
+	private int stars;
+	private Element element;
+	private boolean isEvent;
+	private String story;
+
+	public Player(String[] fields) {
+		this(fields[0], Integer.parseInt(fields[1]), Integer
+				.parseInt(fields[2]), fields[3], Integer.parseInt(fields[4]),
+				fields[5]);
+	}
+
+	public Player(String name, int isHas, int stars, String element,
+			int isEvent, String story) {
+		this.name = name.toUpperCase();
+
+		if (isHas < 0 || isHas > 1) {
+			throw new IllegalArgumentException("Have value cannot be " + isHas);
+		}
+		this.isHas = (isHas == 1);
+
+		if (stars < 1 || stars > 6) {
+			throw new IllegalArgumentException("Cannot have " + stars
+					+ " stars.");
+		}
+		this.stars = stars;
+
+		setElement(element);
+
+		if (isEvent < 0 || isEvent > 1) {
+			throw new IllegalArgumentException("Event value cannot be " + isHas);
+		}
+		this.isEvent = (isEvent == 1);
+
+		this.story = story;
+	}
+
+	private void setElement(String element) {
+		switch (element) {
+		case "FIRE":
+			this.element = Element.FIRE;
+			break;
+		case "WIND":
+			this.element = Element.WIND;
+			break;
+		case "THUNDER":
+			this.element = Element.THUNDER;
+			break;
+		case "LIGHT":
+			this.element = Element.LIGHT;
+			break;
+		case "DARK":
+			this.element = Element.DARK;
+			break;
+		default:
+			throw new IllegalArgumentException(element
+					+ " is an invalid element.");
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean has() {
+		return isHas;
+	}
+
+	public int getStars() {
+		return stars;
+	}
+
+	public boolean isEvent() {
+		return isEvent;
+	}
+
+	public String getElement() {
+		switch (element) {
+		case FIRE:
+			return "FIRE";
+		case WIND:
+			return "WIND";
+		case THUNDER:
+			return "THUNDER";
+		case LIGHT:
+			return "LIGHT";
+		case DARK:
+			return "DARK";
+		default:
+			throw new RuntimeException("No valid element found.");
+		}
+	}
+
+	public String getStory() {
+		return story;
+	}
 }
--- src/soccerspirits/vshue/Service.java
+++ src/soccerspirits/vshue/Service.java
@@ -4,25 +4,37 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Service {
 
-	private final String PLAYER_FILENAME = "players.csv";
-	private final String CSV_DELIMITER = ",";
+	public final String PLAYER_FILENAME = "players.csv";
+	public final String CSV_DELIMITER = ",";
 	private Map<String, Player> playerMap;
 
 	public static void main(String[] args) {
-		Service thisService = new Service();
-		thisService.loadFile();
+		Service service = new Service();
+		service.loadFile();
+	}
+
+	public Service() {
+		playerMap = new HashMap<>();
 	}
 
 	private void loadFile() {
 		try (BufferedReader br = new BufferedReader(new FileReader(
 				PLAYER_FILENAME))) {
+			int linenum = 0;
 			String line = "";
 			while ((line = br.readLine()) != null) {
+				linenum++;
 				String[] fields = line.split(CSV_DELIMITER);
+				if (fields.length != Player.NUM_FIELDS) {
+					System.out.println(linenum + " Invalid: " + line);
+					throw new IOException(linenum + " Invalid: " + line);
+				}
+				playerMap.put(fields[0], new Player(fields));
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -32,5 +44,4 @@ public class Service {
 			e.printStackTrace();
 		}
 	}
-
 }
